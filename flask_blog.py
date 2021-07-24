fb9f48725defea6a562ed57a5f45d82e727afe1f
ellie <ellie@pacifist.studio>
2021-07-24 16:03:40 -0700

    1. Added layout.html; other pages reference this template and override the content block
    2. Added some bootstrap code; this is contained in layout.html
    
    3. Added main.css; this holds styling information, which does not change or update, so it's in a static folder
    
    4. there is some test data to simulate blog posts

--- flask_blog.py
+++ flask_blog.py
@@ -1,16 +1,32 @@
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Ellie Love',
+        'title': 'test post',
+        'content': 'first post content',
+        'date_posted': '20210724'
+    },
+    {
+        'author': 'Ellie Love 2',
+        'title': 'test post 2',
+        'content': 'first post content 2',
+        'date_posted': '20210724'
+    }
+]
 
 
 @app.route('/')
 def hello_world():
-    return "<H1>Hello World</H1>\nEllie is here"
+    return render_template('home.html', posts=posts, title=None)
 
 
 @app.route('/about')
 def about():
-    return '<H1>About</H1>'
+    return render_template('about.html', title='Sexy')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
