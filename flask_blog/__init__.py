15a8eab54ec5d94a0fe563ffccbfe80a029bad0d
ellie <ellie@pacifist.studio>
2021-08-07 19:11:32 -0700

    1. refactor - we're getting more parts to the website, so this refactor adds clearer sections in the file
    2. we're building out a password reset feature, that sends the user an email, and we have to have valid email credentials to do that.  For safety, those credentials should be set on the server as environment variables

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = None
 
-# the three forward slashes is used to specify a relative path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# SECRET KEY ###########################################################################################################
+app.config['SECRET_KEY'] = None
 
 SECRET_KEY_FILE_PATH = 'secret_key'
 with open(SECRET_KEY_FILE_PATH) as infile:
@@ -15,16 +16,40 @@ with open(SECRET_KEY_FILE_PATH) as infile:
     if not app.config['SECRET_KEY']:
         print('WARNING! Could not access secret key')
 
+# DATABASE #############################################################################################################
+
+# the three forward slashes is used to specify a relative path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 # classes are called "models".  Each class in the db is it's own table
 db = SQLAlchemy(app)
+
+# BCRYPT ###############################################################################################################
 
 # we will use bcrypt for hashing passwords, so we don't have to store any raw passwords
 bcrypt = Bcrypt(app)
 
+# LOGIN MANAGER ########################################################################################################
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+# EMAIL ################################################################################################################
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+# note: these are set in the local environment:  /etc/environment
+app.config['EMAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
+app.config['EMAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+
+if not app.config['EMAIL_USERNAME'] or not app.config['EMAIL_PASSWORD']:
+    print('Warning! Could not retrieve email credentials!  Password reset via email will not work!')
+
+mail = Mail(app)
+
 
 # Late Import ########################################################################
 # we have to import this after the db is set up, in order to prevent circular import
