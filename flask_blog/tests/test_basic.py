70db74b120b87c115c01665ab0aab89a696a015e
ellie <ellie@pacifist.studio>
2021-08-18 11:33:51 -0700

    Sample Test

---
+++ flask_blog/tests/test_basic.py
@@ -0,0 +1,17 @@
+import unittest
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test__passing_test(self):
+        self.assertTrue(True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
