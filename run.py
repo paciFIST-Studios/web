121b461339635c08870b72b64069b7c3ca522ac9
paciFIST Studios <ellie@pacifist.studio>
2021-10-15 09:47:04 -0700

    Turn off debug mode

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from flask_blog import create_application
 
-DEBUG=True
+DEBUG=False
 
 if __name__ == '__main__':
+    # how do I push to production?
+    # https://gist.github.com/lemiorhan/8912188
+
     app = create_application()
     app.run(debug=DEBUG)
