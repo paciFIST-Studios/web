927595baddaa44acefc3b254256d84e175a9e633
paciFIST Studios <ellie@pacifist.studio>
2021-10-15 09:04:52 -0700

    comment change

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -42,7 +42,7 @@ def create_application(config_class=Config):
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
-    # minor change 4
+    # minor change: testing to see if this gets pushed to server
 
     return app
 
