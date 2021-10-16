229fcee09d5a4872d551954f58f787619a647855
paciFIST Studios <ellie@pacifist.studio>
2021-10-15 18:44:16 -0700

    remove comments

--- run.py
+++ run.py
@@ -3,8 +3,5 @@ from flask_blog import create_application
 DEBUG=False
 
 if __name__ == '__main__':
-    # how do I push to production?
-    # https://gist.github.com/lemiorhan/8912188
-
     app = create_application()
     app.run(debug=DEBUG)
